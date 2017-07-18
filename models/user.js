401a6adca60a8c570eff767771c1ef67ce34b9aa
Genevieve Partida <gipartida@gmail.com>
2017-07-18 14:54:44 -0700

    authentication working again

--- models/user.js
+++ models/user.js
@@ -8,15 +8,14 @@ const
         password: String
      },
       posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
-    }),
-    User = mongoose.model('User', userSchema)
-
-module.exports = User
+    });
 
 userSchema.methods.generateHash = function(password){
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
-}
+};
 
 userSchema.methods.validPassword = function(password){
   return bcrypt.compareSync(password, this.local.password)
-}
+};
+
+module.exports = mongoose.model('User', userSchema);
