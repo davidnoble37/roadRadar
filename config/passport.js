b0bff1e85c6ae36db6f691fc095557a099a5403e
Brian Montellano <bmontellano@gmail.com>
2017-07-20 18:45:15 -0700

    still trying Heroku

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const
   passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
-  User = require('../models/User')
+  User = require('../models/User.js')
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
